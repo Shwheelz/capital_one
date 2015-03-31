a4537936002c2d53777f7d08a74107ac22cce7aa
Tom Pazamickas <tpzap@yahoo.com>
2015-03-31 15:52:53 -0400

    Fixed the POST requests

--- lib/capital_one.rb
+++ lib/capital_one.rb
@@ -4,6 +4,75 @@ require 'uri'
 
 class CapitalOne
 
+=begin 
+Depending on the access level/type of API key supplied, some of
+these methods will return 403 error codes(forbidden). Enterprise level
+API keys have access to any/all of the GET requests, but nothing else. 
+Customer level API keys have access to GET, POST, PUT, and DELETE, but only
+for valid accounts which are associated with the customer API key. 
+=end
+
+=begin
+Testing data
+DELETION ACCT ID(because I don't want to
+break all the other testing data): 546cd56d04783a02616859c9
+Note: This won't actually delete anything as it isn't a valid customer account ID associated with the API key included here. 
+Customer ID: 5516c07ba520e0066c9ac53b
+^That customer's bank account ID: 55173197c5749d260bb8d151	
+More cust ids w/ account ids:
+cust: 5516c07ba520e0066c9ac53b
+acct: 55175197c5749d260bb8d159
+
+cust: 5516c07ba520e0066c9ac53b
+acct: 5517614cc5749d260bb8d160
+
+Json for updating cusomter data: 	
+myJson ='{
+ 		 "address": {
+  		   "street_number": "424 Waupelani Drive",
+    	   "street_name": "",
+  		   "city": "state college",
+   		   "state": "PA",
+    	   "zip": "16801"
+  		}
+	}'
+
+Json for updating Account data:
+{
+	"type":"Savings",
+	"nickname":"test",
+	"rewards":10,
+	"balance":1000,
+	"customer":"5516c07ba520e0066c9ac53b",
+	"_id":"55173197c5749d260bb8d151"
+	}
+
+Json for creating a new account:
+{
+  "type": "Savings",
+  "nickname": "TomP",
+  "rewards": 2,
+  "balance": 300
+}
+
+Json for creating a new bill: 
+{
+  "status": "",
+  "payment_date": "",
+  "recurring_date": 0,
+  "payment_amount": 0
+}
+
+Json for creating a new transaction:
+{
+  "transaction type": "cash",
+  "payee id": "5517614cc5749d260bb8d160",
+  "amount": 50
+}
+
+=end
+#Test Data:
+
 	APIkey = 'CUSTf52dd79967987b3ba94904e83cc26e47'
 	baseURL = 'http://api.reimaginebanking.com:80'
 
@@ -30,7 +99,7 @@ class CapitalOne
 
 		newBillJson = '{
 		  "status": "Pending",
-		  "payment_amount": 10
+		  "payment_amount": 1000
 		}'
 
 		newTranJson = '{
@@ -38,6 +107,8 @@ class CapitalOne
 		  "payee id": "5517614cc5749d260bb8d160",
 		  "amount": 50
 		}'
+
+#End Test Data
 
 		#GET requests
 
@@ -171,10 +242,7 @@ class CapitalOne
 			url = "http://api.reimaginebanking.com:80/customers/#{custID}?key=#{APIkey}"
 			uri = URI.parse(url)
 			myHash = JSON.parse(json)
-			puts(myHash)
 			http = Net::HTTP.new(uri.host, uri.port)
-			puts(uri.host)
-			puts(uri.port)
 			key = "?key=#{APIkey}"
 			puts(uri.path+key)
 			request = Net::HTTP::Put.new(uri.path+key)
@@ -190,12 +258,8 @@ class CapitalOne
 			url = "http://api.reimaginebanking.com:80/accounts/#{acctID}?key=#{APIkey}"
 			uri = URI.parse(url)
 			myHash = JSON.parse(json)
-			puts(myHash)
 			http = Net::HTTP.new(uri.host, uri.port)
-			puts(uri.host)
-			puts(uri.port)
 			key = "?key=#{APIkey}"
-			puts(uri.path+key)
 			request = Net::HTTP::Put.new(uri.path+key)
 			request.set_form_data(myHash)
 			http.request(request)
@@ -211,48 +275,41 @@ class CapitalOne
 			url = "http://api.reimaginebanking.com:80/customers/#{custID}/accounts?key=#{APIkey}"
 			uri = URI.parse(url)
 			http = Net::HTTP.new(uri.host, uri.port)
-			puts(uri.host)
-			puts(uri.port)
-			key = "?key=#{APIkey}"
-			puts(uri.path+key)
-			myHash = JSON.parse(json)
-		#	getCustAccts('5516c07ba520e0066c9ac53b')
+			request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' =>'application/json'})
+			request.body = json
+			resp = http.request(request)
+			puts(resp.body)
+			getCustAccts('5516c07ba520e0066c9ac53b')
 		end
-		#getCustAccts("5516c07ba520e0066c9ac53b")
 
-		#createAcct('5516c07ba520e0066c9ac53b', newAcctJson)
+		
 
 		#create a new bill on an associated account ID
-		# def self.createBill(acctID, json)
-		# 	url = "http://api.reimaginebanking.com:80/accounts/#{acctID}/bills?key=#{APIkey}"
-		# 	uri = URI.parse(url)
-		# 	http = Net::HTTP.new(uri.host, uri.port)
-		# 	puts(uri.host)
-		# 	puts(uri.port)
-		# 	key = "?key=#{APIkey}"
-		# 	puts(uri.path+key)
-		# 	myHash = JSON.parse(json)
-		# 	puts(myHash)
-		# 	puts(Net::HTTP.post_form(uri, myHash))
-		# 	puts(getAccBills('55173197c5749d260bb8d151'))
-		# end
-		# createBill("55173197c5749d260bb8d151", newBillJson)
+		def createBill(acctID, json)
+			url = "http://api.reimaginebanking.com:80/accounts/#{acctID}/bills?key=#{APIkey}"
+			uri = URI.parse(url)
+			http = Net::HTTP.new(uri.host, uri.port)
+			request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' => 'application/json'})
+			request.body = json
+			resp = http.request(request)
+			puts(resp.body)
+			puts("in create bill")
+			getAccBills(acctID)
+		end
+		
 
 		#create a new transaction between 2 accounts
-		def self.createTransaction(toAcc, json)
+		def createTransaction(toAcc, json)
 			url = "http://api.reimaginebanking.com:80/accounts/#{toAcc}/transactions?key=#{APIkey}"
 			uri = URI.parse(url)
 			http = Net::HTTP.new(uri.host, uri.port)
-			puts(uri.host)
-			puts(uri.port)
-			key = "?key=#{APIkey}"
-			puts(uri.path+key)
-			myHash = JSON.parse(json)
-			puts(myHash)
-			Net::HTTP.post_form(uri, myHash)
-			getAccTransactions('55175197c5749d260bb8d159')
+			request = Net::HTTP::Post.new(uri.request_uri, initheader = {'Content-Type' => 'application/json'})
+			request.body = json
+			resp = http.request(request)
+			puts("Finished Creating Transaction")
+			puts(resp.body)
+			getAccTransactions(toAcc)
 		end
-		#createTransaction('5516c07ba520e0066c9ac53b', newTranJson)
 
 
 		#DELETE requests
